300021a69e0a37e55a2dc3054f02879c931f5d19
maheshDivekar <42762536+maheshDivekar@users.noreply.github.com>
2024-01-20 22:30:18 +0530

    firstcode

---
+++ app.js
@@ -0,0 +1,18 @@
+const heading =React.createElement("h1",{id:"heading"},"Hello world from React from js file !")
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);   
+
+const parent =React.createElement(
+    "div",
+    {id:"parent"},
+  [    React.createElement("div",{id:"child1"},
+  [React.createElement("h1",{},"This is a h1"),
+  React.createElement("h2",{},"This is a h2")
+]) ,    React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"This is a h1"),
+React.createElement("h2",{},"This is a h2")
+]) ]
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); 
+//root.appendChild(parent);
